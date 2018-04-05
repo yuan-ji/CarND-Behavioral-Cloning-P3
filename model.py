da897fb2cf4945b65b65422eda5197b68d74ea40
yuan JI <is.yuan.ji@gmail.com>
2018-04-05 13:54:43 +0000

    ready to add data augmentation

--- model.py
+++ model.py
@@ -3,49 +3,28 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-
-lines = []
-with open('../data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-
+import os
 
 samples = []
-with open('./driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
+    for row in reader:
+        samples.append(row)
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-
-images = []
-measurements = []
-
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-
-
-
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
+        sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
+                name = './data/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
@@ -56,28 +35,29 @@ def generator(samples, batch_size=32):
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
-
-ch, row, col = 3, 80, 320  # Trimmed image format
 
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-0.1)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+# augmented_images, augmented_measurements = [], []
+# for image, measurement in zip(images, measurements):
+#     augmented_images.append(image)
+#     augmented_measurements.append(measurement)
+#     augmented_images.append(cv2.flip(image,1))
+#     augmented_measurements.append(measurement*-0.1)
+
+# X_train = np.array(images)
+# y_train = np.array(measurements)
 
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import MaxPooling2D
 
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+ch, row, col = 3, 80, 320  # Trimmed image format
 
 
 model = Sequential()
@@ -94,7 +74,7 @@ history_object = model.fit_generator(train_generator, samples_per_epoch =
     len(train_samples), validation_data = 
     validation_generator,
     nb_val_samples = len(validation_samples), 
-    nb_epoch=5, verbose=1)
+    nb_epoch=2, verbose=1)
 
 """
 If the above code throw exceptions, try 
